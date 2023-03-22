b9368d802e4aa74d798f7955c26fb987bd235965
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-03-22 22:04:59 +0100

    [mirotalkwebrtc] - #2 make email verification settable from .env

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,6 +12,7 @@ const jwt = require('jsonwebtoken');
 
 const JWT_EXP = process.env.JWT_EXP;
 const JWT_KEY = process.env.JWT_KEY;
+const EMAIL_VERIFICATION = process.env.EMAIL_VERIFICATION === 'true' || false;
 
 //CREATE: /api/v1/user
 
@@ -24,13 +25,29 @@ router.post('/user', validator, async (req, res) => {
             const token = jwt.sign({ username: username, email: email, password: password }, JWT_KEY, {
                 expiresIn: JWT_EXP,
             });
-            //New user, send email confirmation
-            const confirmationCode = `?token=${token}`;
-            nodemailer.sendConfirmationEmail(username, email, confirmationCode);
-            console.log('New user, sent email confirmation');
-            return res.status(201).send({
-                message: '⚠️ Pending account. <br/> Please verify your email to confirm!',
-            });
+            if (EMAIL_VERIFICATION) {
+                //New user, send email confirmation
+                const confirmationCode = `?token=${token}`;
+                nodemailer.sendConfirmationEmail(username, email, confirmationCode);
+                console.log('New user, sent email confirmation');
+                return res.status(201).send({
+                    message: '⚠️ Pending account. <br/> Please verify your email to confirm!',
+                });
+            } else {
+                //New user, no email verification needed, going to add it the db
+                const encryptedPassword = await bcrypt.hash(password, 10);
+                const userData = new User({
+                    email: email,
+                    username: username,
+                    password: encryptedPassword,
+                    token: token,
+                    active: true,
+                    createdAt: new Date().toISOString(),
+                });
+                const userSaveData = await userData.save();
+                console.log('User create OK', userSaveData);
+                res.status(201).json(userSaveData);
+            }
         } else {
             console.log('User already exist');
             res.status(409).json({ message: 'User already exist!' });
@@ -46,36 +63,59 @@ router.post('/user', validator, async (req, res) => {
 router.post('/user/login', validator, async (req, res) => {
     try {
         const { username, email, password } = req.body;
+        const dateNow = new Date().toISOString();
+        const token = jwt.sign({ email: email, username: username, password: password }, JWT_KEY, {
+            expiresIn: JWT_EXP,
+        });
         const userFindOne = await User.findOne({ email: email, username: username });
 
-        if (userFindOne && Object.keys(userFindOne).length != 0 && userFindOne.active) {
-            //User found, but we going to check if the provided password exists
+        if (!Object.is(userFindOne, null) && userFindOne.active) {
+            console.log('User found, but we going to check if the provided password exists');
             bcrypt.compare(password, userFindOne.password, async function (err, result) {
                 if (err) {
                     console.error('login password check', err);
                     res.status(400).json({ message: err });
                 }
-                const dateNow = new Date().toISOString();
-                const token = jwt.sign({ email: email, username: username, password: password }, JWT_KEY, {
-                    expiresIn: JWT_EXP,
-                });
                 if (result) {
-                    //User found, just refresh the token
+                    console.log('User found, just refresh the token');
                     userFindOne.token = token;
                     userFindOne.updatedAt = dateNow;
                     const saveUserFindOne = await userFindOne.save();
                     console.log('User login OK', saveUserFindOne);
                     res.status(201).json(saveUserFindOne);
                 } else {
-                    //New user, send email confirmation
-                    const confirmationCode = `?token=${token}`;
-                    nodemailer.sendConfirmationEmail(username, email, confirmationCode);
-                    console.log('User login, sent email confirmation');
+                    console.log('User found, wrong password!');
                     return res.status(201).send({
-                        message: '⚠️ Pending account. <br/> Please verify your email to confirm!',
+                        message: '⚠️ Account already exists. <br/> The password seems wrong!',
                     });
                 }
             });
+        } else {
+            if (EMAIL_VERIFICATION) {
+                console.log('New user, send email confirmation');
+                const confirmationCode = `?token=${token}`;
+                nodemailer.sendConfirmationEmail(username, email, confirmationCode);
+                console.log('User login, sent email confirmation');
+                return res.status(201).send({
+                    message: '⚠️ Pending account. <br/> Please verify your email to confirm!',
+                });
+            } else {
+                console.log('No email verification, add user to storage...');
+                const encryptedPassword = await bcrypt.hash(password, 10);
+                const userData = new User({
+                    email: email,
+                    username: username,
+                    password: encryptedPassword,
+                    token: token,
+                    active: true,
+                    createdAt: new Date().toISOString(),
+                });
+                const userSaveData = await userData.save();
+                console.log('User create OK', userSaveData);
+                return res.status(201).send({
+                    message: '👍 Account created! <br/> Click on Login and enjoy!',
+                });
+            }
         }
     } catch (error) {
         console.error('login', error);
@@ -93,7 +133,7 @@ router.get('/user/confirmation', auth, async (req, res) => {
         console.log('User confirmation token decoded', decoded);
         const userFindOne = await User.findOne({ email: decoded.email, username: decoded.username });
         if (!userFindOne || Object.keys(userFindOne).length === 0) {
-            //User confirmed by email, going to add it the db
+            //User confirmed by email, going to add it the storage
             const encryptedPassword = await bcrypt.hash(decoded.password, 10);
             const userData = new User({
                 email: decoded.email,
