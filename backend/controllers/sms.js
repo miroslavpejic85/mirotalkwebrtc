979dd8334fc69388bbe9df2ecb92425d48a0c34d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-07-13 15:26:49 +0200

    [mirotalkwebrtc] - fix typo

--- backend/controllers/sms.js
+++ backend/controllers/sms.js
@@ -4,7 +4,11 @@ const smsEnabled = process.env.TWILIO_SMS == 'true';
 const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+
+let client = null;
+if (smsEnabled) {
+    client = require('twilio')(accountSid, authToken);
+}
 
 async function smsSend(req, res) {
     if (!smsEnabled) {
