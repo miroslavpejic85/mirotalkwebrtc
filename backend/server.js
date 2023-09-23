9cd894994e87a90011ad333dd07563c209109867
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-09-23 14:36:46 +0200

    [mirotalkwebrtc] - add nodemon, update dep, minor fix

--- backend/server.js
+++ backend/server.js
@@ -114,8 +114,14 @@ mongoose.connection.on('disconnected', () => {
 });
 
 process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-        console.log('Mongoose connection disconnected through app termination');
-        process.exit(0);
-    });
+    mongoose.connection
+        .close()
+        .then(() => {
+            console.log('Mongoose connection disconnected through app termination');
+            process.exit(0);
+        })
+        .catch((error) => {
+            console.error('Error closing MongoDB connection:', error);
+            process.exit(0);
+        });
 });
