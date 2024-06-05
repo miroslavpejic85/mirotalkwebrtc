39ae6f97c2a838e538c08ed44cde19c9bff76bdb
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-06-05 11:56:26 +0200

    [mirotalkwebrtc] - replace yamljs (deprecated) with js-yaml

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const path = require('path');
-const yaml = require('yamljs');
+const yaml = require('js-yaml');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = yaml.load(path.join(__dirname, '../', 'api/swagger.yaml'));
+const swaggerDocument = yaml.load(fs.readFileSync(path.join(__dirname, '/../api/swagger.yaml'), 'utf8'));
 
 router.use('/docs', swaggerUi.serve);
 router.get('/docs', swaggerUi.setup(swaggerDocument));
