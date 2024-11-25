d84190f6b08186024a30ab41b6107f62f1b2704b
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-11-25 18:56:59 +0100

    [mirotalkwebrtc] - fix room model

--- backend/models/room.js
+++ backend/models/room.js
@@ -3,14 +3,16 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
-    userId: { type: String, index: { unique: true } },
+    userId: { type: String, index: true },
     type: { type: String, enum: ['P2P', 'SFU', 'C2C', 'BRO'] },
     tag: { type: String },
     email: { type: String },
     phone: { type: String },
     date: { type: String },
     time: { type: String },
-    room: { type: String, index: { unique: true } },
+    room: { type: String, index: true },
 });
+
+roomSchema.index({ type: 1, room: 1 }, { unique: true });
 
 module.exports = mongoose.model('Room', roomSchema);
