c0b33e7c077a963ab0e5a0dabc34a89ce0d978c1
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-07-03 15:40:24 +0200

    [mirotalkwebrtc] - fix sentry

--- backend/common/sentry.js
+++ backend/common/sentry.js
@@ -1,26 +1,43 @@
 'use strict';
 
+const SentryNode = require('@sentry/node');
 const logs = require('../common/logs');
 const log = new logs('Sentry');
 
+const SENTRY_ENABLED = process.env.SENTRY_ENABLED === 'true';
 const SENTRY_DSN = process.env.SENTRY_DSN;
-const SENTRY_TRACES_SAMPLE_RATE = process.env.SENTRY_TRACES_SAMPLE_RATE;
+const SENTRY_TRACES_SAMPLE_RATE = parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE || '0.0');
+
+function patchConsoleForSentry() {
+    const originalWarn = console.warn;
+    const originalError = console.error;
+
+    console.warn = function (...args) {
+        SentryNode.captureMessage(args.join(' '), 'warning');
+        originalWarn.apply(console, args);
+    };
+
+    console.error = function (...args) {
+        args[0] instanceof Error
+            ? SentryNode.captureException(args[0])
+            : SentryNode.captureException(new Error(args.join(' ')));
+        originalError.apply(console, args);
+    };
+}
 
 function start() {
-    if (SENTRY_DSN != '') {
-        const sentry = require('@sentry/node');
+    if (SENTRY_ENABLED && typeof SENTRY_DSN === 'string' && SENTRY_DSN.trim()) {
         log.info('Sentry monitoring started...');
-        sentry.init({
+
+        SentryNode.init({
             dsn: SENTRY_DSN,
-            integrations: [
-                sentry.captureConsoleIntegration({
-                    levels: ['warn', 'error'],
-                }),
-            ],
             tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
         });
-        // log.error('TEST error message');
-        // log.warn('TEST warn message');
+
+        patchConsoleForSentry();
+
+        // log.error('Sentry error', { foo: 'bar' });
+        // log.warn('Sentry warning');
     }
 }
 
