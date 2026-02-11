e720c1ed18f12675a534430a1ef8c555965f568d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-02-11 03:45:20 +0100

    [mirotalkwebrtc] - fix typo, update dep

--- frontend/js/stats.js
+++ frontend/js/stats.js
@@ -1,16 +1,15 @@
 'use strict';
 
-// https://github.com/mikecao/umami
-
 const umami = true;
+
+// https://github.com/mikecao/umami
 
 if (!umami) {
     console.warn('Umami is disabled');
-    return;
+} else {
+    const script = document.createElement('script');
+    script.setAttribute('async', '');
+    script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
+    script.setAttribute('data-website-id', 'fbaf621d-b2da-4b33-a2f2-43f8f0037663');
+    document.head.appendChild(script);
 }
-
-const script = document.createElement('script');
-script.setAttribute('async', '');
-script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
-script.setAttribute('data-website-id', 'fbaf621d-b2da-4b33-a2f2-43f8f0037663');
-document.head.appendChild(script);
